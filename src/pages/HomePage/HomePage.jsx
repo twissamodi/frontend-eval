7c45f1479d336fef116e1a6f2b4b0d2f52e61330
Twissa Modi <Twissa_Modi@mckinsey.com>
2022-03-21 11:18:13 +0530

    feat: add home page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from '../../components/Button/Button';
+import Header from '../../components/Header/Header';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+      <section className="homepage-content-section">
+        <h2 className="homepage-content">
+          :((
+          <br />
+          seems a bit empty here...
+        </h2>
+        <Button action="sync" />
+      </section>
+
+    </div>
+  );
+}
+
+export default HomePage;
